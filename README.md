2c376a8a2fa8380becd9c68544a22c44d296b9e0
Deepankar Bhade <deep14bhade@gmail.com>
2022-02-19 17:26:07 +0530

    feat: add public demo link

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Video chat app with Remix and 100ms React SDK in less than 100 lines of code.
 
+Try out the [Public demo](https://remix-video-chat.vercel.app/meeting/6210d32e71bd215ae0423a3c/host)
+
 [Read this Detailed blog](http://dpnkr.in/blog/remix-video-chat-app) for understanding the code and setup.
 
 [![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FDeep-Codes%2Fremix-video-chat&env=HMS_TOKEN_ENDPOINT&envDescription=Token%20endpoint%20can%20be%20found%20in%20developer%20section%20of%20100ms%20Dashboard.&envLink=https%3A%2F%2Fdashboard.100ms.live%2Fdeveloper)
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,9 +12,32 @@ export default function Index() {
         Video chat app with Remix and 100ms react sdk in less than 100 lines of
         code.
       </p>
-      <a href=''>Read the Blog</a>
-      <a href=''>See the Code</a>
-      <a href='https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FDeep-Codes%2Fremix-video-chat&env=HMS_TOKEN_ENDPOINT&envDescription=Token%20endpoint%20can%20be%20found%20in%20developer%20section%20of%20100ms%20Dashboard.&envLink=https%3A%2F%2Fdashboard.100ms.live%2Fdeveloper'>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='http://dpnkr.in/blog/remix-video-chat-app'
+      >
+        Read the Blog
+      </a>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://github.com/Deep-Codes/remix-video-chat/'
+      >
+        See the Code
+      </a>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://remix-video-chat.vercel.app/meeting/6210d32e71bd215ae0423a3c/host'
+      >
+        Try the Public Demo
+      </a>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FDeep-Codes%2Fremix-video-chat&env=HMS_TOKEN_ENDPOINT&envDescription=Token%20endpoint%20can%20be%20found%20in%20developer%20section%20of%20100ms%20Dashboard.&envLink=https%3A%2F%2Fdashboard.100ms.live%2Fdeveloper'
+      >
         <img src='https://vercel.com/button' alt='Deploy with Vercel' />
       </a>
     </div>
